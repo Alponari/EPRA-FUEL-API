c833147a7a463295ae57d3b54c7dc95ef6c9be0f
alponari <alponarijoseph@gmail.com>
2026-07-09 11:55:38 +0300

    Extract EPRA fuel table data from the downloaded HTML

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 url = "https://www.epra.go.ke/pump-prices"
 
 response = requests.get(url)
+response.raise_for_status()
 
 soup = BeautifulSoup(response.text, "html.parser")
-links = soup.find_all("a")
 
-for link in links:
-	text = link.get_text(strip=True)
-	href = link.get("href")
-	
-	print(f"Text: {text}")
-	print(f"Link: {href}")
-	print("_________")
+table = soup.find("table")
 
+if table:
+    rows = table.find_all("tr")
+    headers = [th.get_text(strip=True) for th in rows[0].find_all(["th", "td"])]
+    print(headers)
+
+
+    for row in rows[1:]:
+        columns = [td.get_text(strip=True) for td in row.find_all("td")]
+
+        if columns:
+            print(columns)
+else:
+    print("No table found.")
